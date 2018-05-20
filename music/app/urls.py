b4a20a1f0c6c25ddf47cbfd1aaa636e2d49b6801
shreyanshdwivedi <shreyanshdwivedi1997@gmail.com>
2018-05-20 17:25:29 +0530

    Task Completed

--- music/app/urls.py
+++ music/app/urls.py
@@ -7,4 +7,7 @@ app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^addAlbum$', views.addAlbum, name='addAlbum'),
+    url(r'^search$', views.search, name='search'),
+    url(r'^album/(?P<id>\w+)$', views.album, name='album'),
+    url(r'^addSong/(?P<id>\w+)$', views.addSong, name='addSong'),
 ]
--- music/app/views.py
+++ music/app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.files.storage import FileSystemStorage
 from django.urls import reverse
 from .models import *
+from django.db.models import Q
 
 # Create your views here.
 
@@ -28,3 +29,35 @@ def addAlbum(request):
     else:
         return render(request, 'addAlbum.html', {})
 
+def album(request, id):
+    album = Album.objects.get(id=id)
+    songs = Song.objects.filter(album=album)
+    return render(request, 'album.html', {'album':album, 'songs':songs})
+
+def addSong(request, id):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        album = Album.objects.get(id=id)
+
+        audio = request.FILES['audio']
+        fs = FileSystemStorage()
+        filename_audio = fs.save(audio.name, audio)
+        uploaded_audio_url = fs.url(filename_audio)
+
+        song = Song.objects.create(title=title, audio=audio, album=album)
+        song.save()
+
+        return redirect('/album/'+id)
+    else:
+        return redirect('/album/'+id)
+
+def search(request):
+    if request.method == "GET":
+        searchText = request.GET.get('search')
+        print(searchText)
+        albums = Album.objects.filter()
+        songResults = Song.objects.filter(Q(title__icontains=searchText))
+        albumResults = Album.objects.filter(Q(title__icontains=searchText) | Q(artist__icontains=searchText) | Q(genre__icontains=searchText))
+        searchCount = songResults.count() + albumResults.count()
+        return render(request, 'search.html', {'albums':albums, 'songResults':songResults, 'albumResults':albumResults, 'searchCount':searchCount})
+    return render(request, 'index.html', {})
